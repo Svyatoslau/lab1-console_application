273c6a55104498c40733d00420f77fa003f5453f
Svyatoslav Yaroshevich <svyatyar2@gmail.com>
2020-10-11 22:23:42 +0300

    ADD: Interface: Consumable, Abstract Class: Food

--- src/bsu/rfe/java/group10/lab1/Yaroshevich/varC2/MainApplication.java
+++ src/bsu/rfe/java/group10/lab1/Yaroshevich/varC2/MainApplication.java
@@ -1,8 +1,9 @@
 package bsu.rfe.java.group10.lab1.Yaroshevich.varC2;
 
-public class Main {
+public class MainApplication {
 
     public static void main(String[] args) {
 	// write your code here
+
     }
 }
