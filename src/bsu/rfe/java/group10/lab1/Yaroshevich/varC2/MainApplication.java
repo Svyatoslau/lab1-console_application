91bfc3e0abde17c77e93ca11421f7886b3b31ff4
Svyatoslav Yaroshevich <svyatyar2@gmail.com>
2020-10-13 20:51:58 +0300

    ADD: counting food, in Class MainApplication

--- src/bsu/rfe/java/group10/lab1/Yaroshevich/varC2/MainApplication.java
+++ src/bsu/rfe/java/group10/lab1/Yaroshevich/varC2/MainApplication.java
@@ -12,23 +12,42 @@ public class MainApplication {
         int numFood=0;
         for (String arg: args){
             try {
-                String[] parts = arg.split("/");
-                Class myClass = Class.forName("bsu.rfe.java.group10.lab1.Yaroshevich.varC2." + parts[0]);
-                if (parts.length == 1){
-                    Constructor constructor =myClass.getConstructor();
-                    breakfast[numFood]=(Food)constructor.newInstance();
-                    numFood++;
-                } else
+                if(arg.startsWith("-")){
+                    String[] parts = arg.split("/");
+                    Class myClass = Class.forName("bsu.rfe.java.group10.lab1.Yaroshevich.varC2." + parts[1]);
+                    if (parts.length == 1){
+                        Constructor constructor =myClass.getConstructor();
+                        breakfast[numFood]=(Food)constructor.newInstance();
+                        numFood++;
+                    } else
                     if(parts.length==2){
                         Constructor constructor =myClass.getConstructor(String.class);
-                        breakfast[numFood]=(Food)constructor.newInstance(parts[1]);
+                        breakfast[numFood]=(Food)constructor.newInstance(parts[2]);
                         numFood++;
                     }else
-                        if(parts.length==3){
-                            Constructor constructor= myClass.getConstructor(String.class,String.class);
-                            breakfast[numFood]=(Food)constructor.newInstance(parts[1],parts[2]);
-                            numFood++;
-                        }else throw new NoSuchMethodException();
+                    if(parts.length==3){
+                        Constructor constructor= myClass.getConstructor(String.class,String.class);
+                        breakfast[numFood]=(Food)constructor.newInstance(parts[2],parts[3]);
+                        numFood++;
+                    }else throw new NoSuchMethodException();
+                }
+                else {
+                    String[] parts = arg.split("/");
+                    Class myClass = Class.forName("bsu.rfe.java.group10.lab1.Yaroshevich.varC2." + parts[0]);
+                    if (parts.length == 1) {
+                        Constructor constructor = myClass.getConstructor();
+                        breakfast[numFood] = (Food) constructor.newInstance();
+                        numFood++;
+                    } else if (parts.length == 2) {
+                        Constructor constructor = myClass.getConstructor(String.class);
+                        breakfast[numFood] = (Food) constructor.newInstance(parts[1]);
+                        numFood++;
+                    } else if (parts.length == 3) {
+                        Constructor constructor = myClass.getConstructor(String.class, String.class);
+                        breakfast[numFood] = (Food) constructor.newInstance(parts[1], parts[2]);
+                        numFood++;
+                    } else throw new NoSuchMethodException();
+                }
             }
             catch (NoSuchMethodException e) {
                 System.out.println(arg+" не может быть включён в завтрак");
@@ -40,19 +59,25 @@ public class MainApplication {
             }//исключение для myClass
         }//Java reflection API
 
-        /*int[] num = new int[20];//процедура подсчёта с методом equals()
+        int[] num = new int[20];
         for(int i=0;i<num.length;i++)
             num[i]=i;
-
-        for(int i=0;i<breakfast.length;i++)
-        {
-
-            if(breakfast[i]!=null){
+        for(int i=0;i<breakfast.length;i++) {
+            if(breakfast[i]!=null && num[i]!=-1){
                 num[i]=-1;
-                for(int k=i+1;k<)
-
+                int countFoot=1;
+                for(int k=i+1;k<breakfast.length;k++)
+                {
+                    if(breakfast[i].equals(breakfast[k])){
+                        num[k]=-1;
+                        countFoot++;
+                    }
+                }
+                System.out.print("В колличестве "+countFoot+": ");
+                breakfast[i].consume();
             }
-        }*/
+        }//процедура подсчёта с методом equals()
+
 
     }
 }
